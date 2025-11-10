453192367cd7310ebdd0b398011140243e999979
Lily Autumn <realprismaticyt@gmail.com>
2025-11-10 20:17:00 +1300

    Who up bootstrapping they class dumper

--- src/main/java/xyz/lilyflower/lilytweaks/bootstrap/LilyflowerTweaksBootstrapSystem.java
+++ src/main/java/xyz/lilyflower/lilytweaks/bootstrap/LilyflowerTweaksBootstrapSystem.java
@@ -1,17 +1,26 @@
 package xyz.lilyflower.lilytweaks.bootstrap;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.lang.instrument.Instrumentation;
 import java.lang.management.ManagementFactory;
+import java.nio.file.Paths;
+import java.security.ProtectionDomain;
 import java.util.List;
+import net.bytebuddy.agent.ByteBuddyAgent;
 import net.minecraft.launchwrapper.ITweaker;
 import net.minecraft.launchwrapper.LaunchClassLoader;
+import org.apache.commons.lang3.ClassUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 // How early can we go?
 @SuppressWarnings("unused")
 public class LilyflowerTweaksBootstrapSystem implements ITweaker {
-    public static final Logger LOGGER = LogManager.getLogger("Lilyflower Tweaks Bootstrap System");
+    public static final Logger LOGGER = LogManager.getLogger("Lilyflower Tweaks Bootstreap System");
 
     @Override
     public void acceptOptions(List<String> args, File gameDir, File assetsDir, String profile) {
@@ -37,7 +46,19 @@ public class LilyflowerTweaksBootstrapSystem implements ITweaker {
         long pid = Long.parseLong(name.split("@")[0]);
         LOGGER.info("Spinning up...");
         LOGGER.info("Process ID: {}", pid);
-        LOGGER.info(LilyflowerTweaksBootstrapSystem.class.getClassLoader().getClass().getCanonicalName());
-        System.exit(1);
+        Instrumentation agent = ByteBuddyAgent.install();
+        agent.addTransformer(new ClassDumper(), true);
+    }
+
+    public static class ClassDumper implements ClassFileTransformer {
+        @Override
+        public byte[] transform(ClassLoader loader, String name, Class<?> target, ProtectionDomain domain, byte[] bytes) {
+            String readable = ClassUtils.getShortClassName(name).replaceAll("/", "∕");
+            File clazz = new File("classes/" + readable.replaceAll("\\.", "$") + ".class");
+            try (FileOutputStream output = new FileOutputStream(clazz)) {
+                output.write(bytes);
+            } catch (IOException ignored) {}
+            return bytes;
+        }
     }
 }
